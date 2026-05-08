68d200c307451041e764696b01315c424998e139
posicionaando-maker <posiciona.ando@gmail.com>
2026-05-08 02:08:51 -0400

    Add files via upload

---
+++ sw-catalogo.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'catalogo-v2';
+const urlsToCache = [
+  '/catalogo/',
+  '/catalogo/index.html',
+  '/catalogo/manifest-catalogo.json',
+  '/catalogo/inventario.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+});
